4b756e971c5b3c1412f6b613bd0720e1b4f9591c
Vitaly Sokolov <sokoloff06@gmail.com>
2018-11-28 17:02:57 +0200

    Added nav drawer in both activites and resolved some bugs with the selection in it

--- app/src/main/java/com/example/sokol/mcprices/MainActivity.java
+++ app/src/main/java/com/example/sokol/mcprices/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.sokol.mcprices;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.appsflyer.AppsFlyerConversionListener;
@@ -19,6 +26,7 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity implements CartHandler {
 
     private static final String AF_DEV_KEY = "4UGrDF4vFvPLbHq5bXtCza";
+    NavigationView navigationView;
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -28,13 +36,13 @@ public class MainActivity extends AppCompatActivity implements CartHandler {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private MenuCartPagerAdapter pagerAdapter;
-
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager viewPager;
     private Cart cart;
     private CartAdapter cartAdapter;
+    private DrawerLayout navDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +62,39 @@ public class MainActivity extends AppCompatActivity implements CartHandler {
 
         TabLayout tabLayout = findViewById(R.id.tabs_selector);
         tabLayout.setupWithViewPager(viewPager);
+
+        navDrawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+
+                        navDrawer.closeDrawers();
+
+                        int itemId = menuItem.getItemId();
+                        switch (itemId) {
+                            case R.id.nav_sale:
+                                Intent intent = new Intent(getApplicationContext(), SaleActivity.class);
+                                startActivity(intent);
+                                return true;
+                        }
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
         AppsFlyerConversionListener conversionListener = new AppsFlyerConversionListener() {
             @Override
@@ -78,27 +119,30 @@ public class MainActivity extends AppCompatActivity implements CartHandler {
 
             }
         };
+
+        String senderId = "145246440594"; /* A.K.A Project Number */
+        AppsFlyerLib.getInstance().enableUninstallTracking(senderId);
+
         AppsFlyerLib.getInstance().init(AF_DEV_KEY, conversionListener, getApplicationContext());
         AppsFlyerLib.getInstance().startTracking(getApplication());
     }
 
-
-/*    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                navDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == (R.id.acton_refresh)) {
-            MenuFragment mf = (MenuFragment) pagerAdapter.getItem(0);
-            mf.startUpdate();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
+    protected void onResume() {
+        super.onResume();
+        navigationView.setCheckedItem(R.id.nav_main);
+    }
+
 
 
     @Override
